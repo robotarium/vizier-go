b25434459ef993d1af8fbfe9348db827e2c6fc2c
pglotfel <pglotfel@gmail.com>
2018-10-18 16:17:47 -0400

    initial commit

---
+++ vizier/utils.go
@@ -0,0 +1,9 @@
+package vizier
+
+func ParseDescriptor([]byte descriptor) map[string]interface{} {
+	
+}
+
+func parseDescriptorHelper(path string, link string, body *map[string]interface{}) map[string]interface{} {
+
+}
